aa92b2a0c7f238d38f06f9387dedda8926f2a7f7
Maia <maiaaizner@gmail.com>
2024-11-05 11:50:12 -0300

    feat: change states in both components and ways to do it: ClassState and UseState

--- src/ClassState.js
+++ src/ClassState.js
@@ -1,26 +1,73 @@
+//Estados compuestos en componentes con clases
 import React from 'react';
+import { Loading } from './Loading';
 
+const SECURITY_CODE = "paradigma";
 class ClassState extends React.Component {
     //Esta es la forma de extender a la super clase (clase padre de react) y editarla
     constructor(props) {
         super(props)
-        this.state = {
-            error: false, 
-        }
 
+        this.state = {
+              error: false,
+            loading: false,
+            value: "",
+        }
+    }
+    
+    // UNSAFE_componentWillMount(){
+    //     console.log('componentWillMount')  
+    // }
+  
+    // componentDidMount(){
+    //     console.log('componentDidMount')
+    // }
+    componentDidUpdate() {
+        console.log('actualizacion');
+
+        if( !!this.state.loading) {
+
+            setTimeout( () => {
+                console.log('haciendo la validacion');
+                if (SECURITY_CODE  === this.state.value){
+
+                    this.setState({ error:false,loading:false });
+                }else {
+                    this.setState({ error:true, loading:false });
+                    
+                }
+                console.log('terminando validacion');
+            }, 3000)
+        }
     }
 
     render() {
+        const {name, error, loading, value} = this.state
         return (
             <div>
-                <h2>Eliminar {this.props.name}</h2>
+                <h2>Eliminar {name}</h2>
                 <p>Por favor, escribe el código de seguridad . </p>
-                {this.state.error && (
+
+                {(error && !loading)  && (
                     <p>Error: El código es incorrecto</p>
                 )}
-                <input placeholder='código de seguridad'/>
+
+                {loading && (
+                    <Loading />
+                )}
+
+                <input 
+                    placeholder='código de seguridad'
+                    value = { value }
+                    onChange={ (e) => {
+                        this.setState({value: e.target.value});
+                    }}
+                />
                 <button 
-                onClick={() => this.setState({ error: !this.state.error })}
+                    onClick={() => this.setState({ 
+                        // error: !this.state.error
+                       loading:true
+                    })}
                 >Comprobar</button>
             </div>
         );
--- src/UseState.js
+++ src/UseState.js
@@ -1,18 +1,52 @@
 import React from "react";
 
+const SECURITY_CODE = 'paradigma';
+
 function UseState({name}) {
     //Manejo del estado de forma individual y declarativa
+    const [value, setValue] = React.useState('');
     const [error, setError] = React.useState(false);
+    const [loading, setLoading] = React.useState(false);
+
+    React.useEffect(() => {
+        console.log ('empezando efecto')
+
+        if (loading) {
+            //funcion protege el disparador de cambio de estado de loading 
+            setTimeout(()=> {
+                //console.log( 'haciendo validacion');
+                setError(false)
+                if (value !== SECURITY_CODE) {
+                    setError(true)
+                }
+                setLoading(false);
+                //console.log( 'terminando validacion')
+            }, 3000)
+
+        }
+
+    }, [loading])
+    
     return (
         <div>
             <h2>Eliminar {name}</h2>
             <p>Por favor, escribe el código de seguridad.</p>
-            {error && (
+            {(error && !loading) && (
                 <p>Error: El código es incorrecto</p>
             )}
-            <input placeholder='código de seguridad'/>
+             {loading && (
+                <p>Cargando ...</p>
+            )}
+            <input 
+                placeholder='código de seguridad'
+                value = {value}
+                onChange={(e) => {
+                    setValue(e.target.value) 
+                }}
+            />
             <button
-            onClick={() => setError(!error)}
+            //entraremos en el efecto
+            onClick={() => setLoading(!loading)}
             >Comprobar</button>
         </div>
     );
